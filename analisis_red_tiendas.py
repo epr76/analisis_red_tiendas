9702a5a148e220560f9ed92c718200ccd9bb5964
Ernest Perea <e.perea76@gmail.com>
2026-09-11 17:01:59 +0200

    01- lectura de los csv

--- analisis_red_tiendas.py
+++ analisis_red_tiendas.py
@@ -1,1 +1,11 @@
-print("hello")
+import numpy as np
+import pandas as pd
+
+
+#csv inventories
+df_csv_inventories = pd.read_csv('inventories.csv')
+#csv sales
+df_csv_sales = pd.read_csv('sales.csv')
+#csv satisfaction
+df_csv_satisfaction = pd.read_csv('satisfaction.csv')
+
